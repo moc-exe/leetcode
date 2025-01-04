697244611644f737044b8628b634e70bd4fba622
moc <lev.ustinov@gmail.com>
2025-01-03 22:45:12 -0500

    welp

---
+++ src/M238_ProductOfArrayExceptSelf.java
@@ -0,0 +1,38 @@
+public class M238_ProductOfArrayExceptSelf {
+    
+    public int[] productExceptSelf(int[] nums) {
+        
+        int len = nums.length;
+        int[] left = new int[len];
+        int[] right = new int[len];
+
+        int l_running = 1;
+        for (int i = 0; i < len; i++) {
+            
+            if(i == 0){
+                left[i] = l_running;
+            }
+            else{
+                l_running *= nums[i-1];
+                left[i] = l_running;
+            }
+        }
+        int r_running = 1;
+        for (int i = len - 1; i >= 0; i--) {
+            if(i == len - 1){
+                right[i] = r_running;
+            }
+            else{
+                r_running *= nums[i+1];
+                right[i] = r_running;
+            }
+
+        }
+        int[] out = new int[len];
+        for (int i = 0; i < len; i++) {
+            out[i] = left[i] * right[i];
+        }
+        return out;
+    }
+
+}
