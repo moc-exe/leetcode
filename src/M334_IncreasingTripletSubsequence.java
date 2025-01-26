1668e1f3db1ab0ca8fa4ec8a804628986ae5d322
moc <lev.ustinov@gmail.com>
2025-01-25 21:26:16 -0500

    solved M334 today

--- src/M334_IncreasingTripletSubsequence.java
+++ src/M334_IncreasingTripletSubsequence.java
@@ -1,23 +1,52 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+
+/*
+ * 
+ * https://leetcode.com/problems/increasing-triplet-subsequence/submissions/1520574478/?envType=study-plan-v2&envId=leetcode-75
+ * 334. Increasing Triplet Subsequence - MEDIUM
+ */
 
 public class M334_IncreasingTripletSubsequence{
 
     public boolean increasingTriplet(int[] nums) {
 
-        HashMap<Integer, Integer[]> map = new HashMap<>();
-        
+        if(nums.length < 3){return false;}
+
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+
         // populate the map
         var max = Integer.MIN_VALUE;
         var min = Integer.MAX_VALUE;
+        //var len = nums.length;
+        
+        // mins
         for (int i = 0; i < nums.length; i++) {
             
-
-
+            min = (nums[i] < min) ? nums[i] : min;
+            var temp_list = new ArrayList<Integer>();
+            temp_list.add(min);
+            map.put(i, temp_list);
+        
         }
 
+        // maxs
+        for (int j = nums.length - 1; j >= 0; j--) {
+            
+            max = (nums[j] > max) ? nums[j] : max;
+            map.get(j).add(max);
+    
+        }
 
+        for (int i = 1; i < nums.length - 1; i++) {
+            
+            var left = map.get(i-1).get(0);
+            var right = map.get(i+1).get(1);
+            if(nums[i] > left && nums[i] < right){return true;}
+        }
 
-        
+        return false;
+
     }
 
 
