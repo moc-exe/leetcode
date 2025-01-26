83655015492c79c6fd64733ef609701a4fc263aa
moc <lev.ustinov@gmail.com>
2025-01-25 21:25:31 -0500

    solved M334 today

---
+++ src/M334_IncreasingTripletSubsequence.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class M334_IncreasingTripletSubsequence{
+
+    public boolean increasingTriplet(int[] nums) {
+
+        HashMap<Integer, Integer[]> map = new HashMap<>();
+        
+        // populate the map
+        var max = Integer.MIN_VALUE;
+        var min = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            
+
+
+        }
+
+
+
+        
+    }
+
+
+}
