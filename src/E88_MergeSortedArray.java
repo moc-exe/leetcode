105da6df8d82bf5f24bceb709e4618d7250358b8
lev <Lev.Ustinov@ec.gc.ca>
2025-10-01 16:21:43 -0400

    [java] solved E88 today

---
+++ src/E88_MergeSortedArray.java
@@ -0,0 +1,43 @@
+public class E88_MergeSortedArray {
+
+    public class Solution {
+        
+        public void merge(int[] nums1, int m, int[] nums2, int n){
+
+            int[] outArr = new int[n+m];
+            int len = n +m;
+            int i= 0, j = 0, k =0;
+            while(i < m && j < n){
+
+                if(nums1[i] <= nums2[j]){
+                    outArr[k] = nums1[i];
+                    i++;
+                }
+                else{
+                    outArr[k] = nums2[j];
+                    j++;
+                }
+                k++;
+            }
+            while(i < m){
+
+                outArr[k] = nums1[i];
+                i++;
+                k++;
+
+            }
+            while (j < n) {
+                outArr[k] = nums2[j];
+                j++;
+                k++;
+            }
+
+            for(int index = 0; index < len; index++){
+                nums1[index] = outArr[index];
+            }
+        }
+
+
+    }
+    
+}
