7e33d4598fcb44e3767f8308c0df8a282ef8f56b
moc <lev.ustinov@gmail.com>
2025-10-03 15:45:22 -0400

    [java] solved M3100 today

---
+++ src/M3100_WaterBottlesII.java
@@ -0,0 +1,45 @@
+public class M3100_WaterBottlesII{
+
+    class Solution {
+        
+        public int maxBottlesDrunk(int numBottles, int numExchange) {
+
+            int fullBottles = numBottles;
+            int emptyBottles = 0;
+            int exchangeRate = numExchange;
+            int drankCounter = 0;
+            int[] temp;
+
+            while(fullBottles > 0){
+
+                drankCounter += fullBottles;
+                emptyBottles += fullBottles;
+                fullBottles = 0;
+                temp = exchange(fullBottles, emptyBottles, exchangeRate);
+                if(temp == null) break;
+                fullBottles = temp[0];
+                emptyBottles = temp[1];
+                exchangeRate = temp[2];
+            }
+            
+            return drankCounter;
+
+        }
+        
+        public int[] exchange(int numFullBottles, int numEmptyBottles, int numExchange){
+
+            if(numEmptyBottles < numExchange) return null;    
+            int[] outArr = new int[3];
+            
+            outArr[1] = numEmptyBottles - numExchange;
+            outArr[0] = numFullBottles+1;
+            outArr[2] = numExchange+1;
+
+            return outArr;
+
+        }
+    
+    }
+
+
+};
